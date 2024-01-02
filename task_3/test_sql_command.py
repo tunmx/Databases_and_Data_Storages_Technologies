8043adb71df23260548d337335b1b86382d60f17
JingyuYan <tunmxy@163.com>
2024-01-02 17:30:25 +0800

    fk

---
+++ task_3/test_sql_command.py
@@ -0,0 +1,24 @@
+import time
+
+from setting import get_engine
+
+engine = get_engine()
+with engine.connect() as connection:
+    select_test = """
+        SELECT * FROM events
+        WHERE event_time >= '2017-06-22 00:00:00'
+          AND event_time < '2017-06-23 00:00:00';
+    """
+    t = time.time()
+    result = connection.execute(select_test)
+    print(result)
+    print(time.time() - t)
+
+    # print("INDEX")
+    # create_index = """CREATE INDEX idx_event_type ON events(event_type);"""
+    # connection.execute(create_index)
+    #
+    # t = time.time()
+    # result = connection.execute(select_test)
+    # print(result)
+    # print(time.time() - t)
